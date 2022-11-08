95a64640f478173b31cdf777b2f7869bd90c6e66
Alan Phan <alanphanxyz@gmail.com>
2022-11-08 16:51:42 +0700

    first todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React, { useEffect, useState } from 'react'
+
+const list = [
+  {
+    id: 1,
+    title: '1st Item',
+    description: 'Description here.',
+  },
+  {
+    id: 2,
+    title: '2nd Item',
+    description: 'Another description here.',
+  },
+  {
+    id: 3,
+    title: '3rd Item',
+    description: 'Third description here.',
+  },
+]
 
 function App() {
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://127.0.0.1:8000/api/`)
+      const data = await response.json()
+      setTodos(data)
+    }
+    fetchData()
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
+      </div>
+      <div>
+        {todos.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
